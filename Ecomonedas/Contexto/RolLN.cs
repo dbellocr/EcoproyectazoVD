3e7076ee5c22198732736ffdcee05b3f5a70e53e
Daniel <dbello0808@gmail.com>
2019-03-03 18:05:42 -0600

    Creación de usuario LN y Rol LN

---
+++ Ecomonedas/Contexto/RolLN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contexto
+{
+   public class RolLN
+    { 
+
+        public IQueryable ListaRoles()
+        {
+            EcomonedasContexto db = new EcomonedasContexto();
+            return db.Rol;
+        }
+
+
+    }
+}
---
+++ Ecomonedas/Contexto/UsuarioLN.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contexto
+{
+    public class UsuarioLN
+    {
+
+        public static int GuardarUsuario()
+        {
+
+
+            return 0;
+
+        }
+
+
+
+    }
+}
